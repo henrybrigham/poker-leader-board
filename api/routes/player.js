29b58eba209fbb4125e4d687ebd4568b0fa4997b
Henry Brigham <hhbrigham5@gmail.com>
2018-11-04 22:15:15 -0700

    fixing upload image url

--- api/routes/player.js
+++ api/routes/player.js
@@ -8,6 +8,14 @@ const PlayerModel = require(`${gar}/models/player.model`);
 // create the multer instance that will be used to upload/save the file
 const upload = multer({ dest: `${gar}/uploads` });
 
+let apiUrl;
+
+if(process.env.NODE_ENV === 'development') {
+	apiUrl = 'http://localhost:8000/uploads/';
+} else {
+	apiUrl = 'http://68.183.110.66:8000/uploads/';
+}
+
 router.get('/', function(req, res) {
 	PlayerModel.find(
 		{ }, // filter object - empty filter catches everything
@@ -41,19 +49,11 @@ router.get('/:playerId', function(req, res) {
 
 router.post('/', upload.single('picture'), function(req, res){
 	let newPlayer = JSON.parse(req.body.player);
-	let apiUrl;
-
-	if(process.env.NODE_ENV === 'development') {
-		apiUrl = 'http://localhost:8000';
-	} else {
-		apiUrl = 'http://68.183.110.66:8000';
-	}
-
+	
 	newPlayer.imageUrl = apiUrl + req.file.filename;
 	
   new PlayerModel(newPlayer).save(function(err, createdPlayer){
 		if (err) {
-			console.log('**error', err);
 			res.status(500);
 			res.send("Error creating player", error);
 			return;
@@ -63,11 +63,10 @@ router.post('/', upload.single('picture'), function(req, res){
 });
 
 router.put('/:playerId', upload.single('picture'), function(req, res){
-	console.log('****player', req.body);
 	let updatedPlayer = JSON.parse(req.body.player);
 	
 	if (req.file) {
-		updatedPlayer.imageUrl = 'http://localhost:8000/uploads/' + req.file.filename;
+		updatedPlayer.imageUrl = apiUrl + req.file.filename;
 	}
 
 	PlayerModel.findOneAndUpdate(
