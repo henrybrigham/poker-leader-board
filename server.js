13248373aabee665e7e0c290c29de449611cd805
Henry Brigham <hhbrigham5@gmail.com>
2018-11-04 20:42:36 -0700

    changing port for prod

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist/'));
-app.listen(8080);
-console.log('listening 8080');
+app.listen(80);
+console.log('listening 80');
